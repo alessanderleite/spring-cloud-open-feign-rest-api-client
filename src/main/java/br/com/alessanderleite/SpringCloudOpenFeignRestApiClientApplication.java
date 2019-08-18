d7917806f1de4dd2edc377425dab30de3fd7f3a2
alessanderleite <alessander.logistica@gmail.com>
2019-08-18 16:43:36 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/SpringCloudOpenFeignRestApiClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenFeignRestApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOpenFeignRestApiClientApplication.class, args);
+	}
+
+}
