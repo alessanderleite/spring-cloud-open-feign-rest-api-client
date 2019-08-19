eca245d50f638f3eb1069ebf7538dbd613825a81
alessanderleite <alessander.logistica@gmail.com>
2019-08-18 23:37:53 -0300

    create the model class named Geolocalizacao.java

---
+++ src/main/java/br/com/alessanderleite/model/Geolocalizacao.java
@@ -0,0 +1,80 @@
+package br.com.alessanderleite.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "geolocalizacao")
+public class Geolocalizacao implements Comparable<Geolocalizacao>{
+	@Id
+	@Column(name = "id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;	
+	
+	private String distance;
+	private String title;
+	private String location_type; 	
+	private String woeid;
+	private String latt_long; 		
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDistance() {
+		return distance;
+	}
+
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLocation_type() {
+		return location_type;
+	}
+
+	public void setLocation_type(String location_type) {
+		this.location_type = location_type;
+	}
+
+	public String getWoeid() {
+		return woeid;
+	}
+
+	public void setWoeid(String woeid) {
+		this.woeid = woeid;
+	}
+
+	public String getLatt_long() {
+		return latt_long;
+	}
+
+	public void setLatt_long(String latt_long) {
+		this.latt_long = latt_long;
+	}
+
+	@Override
+	public int compareTo(Geolocalizacao o) {
+		if (Integer.parseInt(this.distance) <= Integer.parseInt(o.distance)) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+}
