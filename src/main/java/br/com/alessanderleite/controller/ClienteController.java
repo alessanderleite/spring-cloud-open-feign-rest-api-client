cff7c3279ed14afa42158a8a790bcb2743e64d89
alessanderleite <alessander.logistica@gmail.com>
2019-08-21 23:14:44 -0300

    create the methods for update, delete and get client by id

--- src/main/java/br/com/alessanderleite/controller/ClienteController.java
+++ src/main/java/br/com/alessanderleite/controller/ClienteController.java
@@ -5,8 +5,11 @@ import java.net.URISyntaxException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +33,24 @@ public class ClienteController {
 				.body(clienteSave);
 	}
 	
+	@PutMapping()
+	public ResponseEntity<?> atualizar(@RequestBody RetornoVO cliente) {
+		return ResponseEntity.ok().body(clienteBO.atualizar(cliente));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarClienteId(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(clienteBO.buscarClienteId(id));
+	}
+	
 	@GetMapping
 	public ResponseEntity<?> listaClientes() {
 		return ResponseEntity.ok().body(clienteBO.listaClientes());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletar(@PathVariable Integer id) {
+		clienteBO.deletar(id);
+		return ResponseEntity.ok().build();
 	}
 }
